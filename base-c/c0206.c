a817788a38096d876fab561cc2ebea7b2491226f
Awww-nanguasu <2328194470@qq.com>
2022-02-20 17:31:03 +0800

    c4 文件

---
+++ base-c/c0206.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//int main()
+//{
+    //int num=4;
+    //if(num==5)
+        //printf("hehe");
+    //return 0;
+//}
+int main()
+{
+    int a=0;
+    while(a<=100)
+    {
+        if(0!=a%2)
+            printf("%dÊÇÆæÊı\n",a);
+        else
+            ;
+        a++;
+    }
+    return 0;
+}
