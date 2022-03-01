4c39723e0f097c169a906e6bb7dbd6770105a891
Awww-nanguasu <2328194470@qq.com>
2022-03-01 20:22:50 +0800

    找出100-200间的素数

--- Practice-c/c0227.c
+++ Practice-c/c0227.c
@@ -1,21 +1,41 @@
 //输出100-200间的素数
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+//int main()
+//{
+//    int i,count=0;
+//    for(i=100;i<=200;i++)
+//    {
+//        for(count=2;count<i;count++)
+//        {
+//            if(i%count==0)
+//           {
+//                break;
+//            }
+//        }
+//        if(count==i)
+//        {
+//            printf("%d为质数",i);
+//        }
+//    }
+//    return 0;
+//}
 int main()
 {
-    int i,count=0;
-    for(i=100;i<=200;i++)
+    int i,count;
+    for(i=101;i<=200;i+=2)
     {
-        for(count=2;count<i;count++)
+        for(count=2;count<sqrt(i);count++)
         {
             if(i%count==0)
             {
                 break;
             }
         }
-        if(count==i)
+        if(count>sqrt(i))
         {
-            printf("%d为质数",i);
+            printf("%d\n",i);
         }
     }
     return 0;
