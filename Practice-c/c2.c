e8dc69a365c729f5dd57199e8a6b33af243ce50c
Awww-nanguasu <2328194470@qq.com>
2022-03-20 20:32:25 +0800

    初识函数

---
+++ Practice-c/c2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int text()
+{
+    int a=0;
+    a=10;
+    if(a==10)
+    {
+        printf("½áÊøÁË");
+    }
+    return 20;
+}
+int main()
+{
+    printf("%d\n",text());
+    return 0;
+}
