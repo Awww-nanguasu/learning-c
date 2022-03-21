a9335acd4cb7621e279c6d43287f49a1ebca17c1
Awww-nanguasu <2328194470@qq.com>
2022-03-21 21:53:28 +0800

    初识二维数组

---
+++ base-c/c_03_21_.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+int main()
+{
+    int arr[2][2]={0,1,4,5};
+    printf("%d",arr[1][1]);
+}
+//#include<stdio.h>
+//#define M 4
+//#define N 3
+//void main()
+//{
+//    int a[M][N],i,j;
+//    for(i=0;i<M;i++)
+//        for(j=0;j<N;j++)
+//            a[i][j]=3*i+j;
+// 
+//    for(i=0;i<M;i++)
+ //   {   for(j=0;j<N;j++)
+//            printf("%d\t",a[i][j]);//直接用数组来访问
+//        printf("\n");
+//    }
+ 
+//}
