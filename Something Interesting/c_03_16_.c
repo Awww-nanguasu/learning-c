bdae334afa7c5d590875ef6bcc1417ba3118688b
Awww-nanguasu <2328194470@qq.com>
2022-03-25 10:03:42 +0800

    n子棋可PVP版

--- Something Interesting/c_03_16_.c
+++ Something Interesting/c_03_16_.c
@@ -1,10 +1,34 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 #define Win 5   //胜利胜利棋子个数
 #define Jiange 10  //不要问这个是什么，跟Hang和Line保持一致就行了，不然你试试
 #define Line 10
 #define Hang 10
-void DisplayBoard(int locate,int MineBoard[Hang][Line],char set){
+void Loading(int j)
+{
+    int i;
+    char arr[15]="正在退出游戏";
+    char arr1[15]="正在加载游戏";
+    char arr2[1]="~";
+    for ( i = 12;i < 15; i++)
+    {
+        if (j==1)
+        {
+            arr[i]=arr2[0];
+            printf("%s\n",arr);
+        }
+        else if(j==2)
+        {
+            arr1[i]=arr2[0];
+            printf("%s\n",arr1);
+        }
+        Sleep(500);
+        system("cls");
+    }
+}
+void DisplayBoard(int locate,int MineBoard[Hang][Line],char set)
+{
     int i,j,k,count,serial,serialx,serialy;
     count=1;
     serial=(count*10);
@@ -15,12 +39,18 @@ void DisplayBoard(int locate,int MineBoard[Hang][Line],char set){
         {
             serialx=serial/10;
             serialy=serial%10;
-            if (MineBoard[serialx-1][serialy-1]!=0)
+            if (MineBoard[serialx-1][serialy-1]==1)
             {
-                printf("%c ",set);
+                printf("# ");
             }
-            else
+            if (MineBoard[serialx-1][serialy-1]==-1)
+            {
+                printf("* ");
+            }
+            if (MineBoard[serialx-1][serialy-1]==0)
+            {
                 printf("%d",serial);
+            }
             if (j==Line -1)
             {
                 printf("\n");
@@ -37,9 +67,9 @@ void DisplayBoard(int locate,int MineBoard[Hang][Line],char set){
             printf("\n");
         }
     }
-//    printf("下子数组第131个:%d\n",MineBoard[12][0]);
 }
-int IsWin(int MineBoard[Hang][Line]){
+int IsWin(int MineBoard[Hang][Line])
+{
     int hang,lie,k,sum; 
     for ( hang = 0; hang < Hang; hang++)
     {
@@ -60,8 +90,8 @@ int IsWin(int MineBoard[Hang][Line]){
                         printf("玩家1赢!\n");
                         return 1;
                     }
-                    //玩家2对应2倍Win
-                    if (sum==2*Win)
+                    //玩家2对应-1倍Win
+                    if (sum==-Win)
                     {
                         printf("玩家2赢!\n");
                         return 1;
@@ -79,7 +109,7 @@ int IsWin(int MineBoard[Hang][Line]){
                         printf("玩家1赢!\n");
                         return 1;
                     }  
-                    if (sum==2*Win)
+                    if (sum==-Win)
                     {
                         printf("玩家2赢!\n");
                         return 1;
@@ -97,7 +127,7 @@ int IsWin(int MineBoard[Hang][Line]){
                         printf("赢!\n");
                         return 1;
                     } 
-                    if (sum==2*Win)
+                    if (sum==-Win)
                     {
                         printf("玩家2赢!\n");
                         return 1;
@@ -112,10 +142,10 @@ int IsWin(int MineBoard[Hang][Line]){
                     }
                     if (sum==Win)
                     {
-                        printf("赢!\n");
+                        printf("玩家1赢!\n");
                         return 1;
                     }
-                    if (sum==2*Win)
+                    if (sum==-Win)
                     {
                         printf("玩家2赢!\n");
                         return 1;
@@ -126,7 +156,8 @@ int IsWin(int MineBoard[Hang][Line]){
     }
     return 0;
 }
-void game(){
+void game()
+{
     int locate,locatex,locatey,ret=0;
     int count,player=-1;
     char player1='#'; //玩家1棋子
@@ -141,15 +172,7 @@ void game(){
         if (count<=Hang*Line)
         {
             //当下了大于Win个棋后判断输赢
-            if (count>=Win-1)
-            {
-                //判断输赢
-                ret=IsWin(MineBoard);
-                if (ret==1)
-                {
-                    break;
-                }
-            }
+            //判断输赢
             //玩家1,2轮流下棋，此处代码重复，交给你优化了
             if (player==-1)
             {
@@ -161,6 +184,14 @@ void game(){
                 DisplayBoard(locate,MineBoard,player1);
                 count++;
                 player*=-1;
+                if (count>=Win-1)
+                {
+                    ret=IsWin(MineBoard);
+                    if (ret==1)
+                    {
+                        break;
+                    }
+                }
             }
             if (player==1)
             {
@@ -168,22 +199,31 @@ void game(){
                 scanf("%d",&locate);
                 locatex=locate/10;
                 locatey=locate%10;
-                MineBoard[locatex-1][locatey-1]=2;
+                MineBoard[locatex-1][locatey-1]=-1;
                 DisplayBoard(locate,MineBoard,player2);
                 count++;
                 player*=-1;
+                if (count>=Win-1)
+                {
+                    ret=IsWin(MineBoard);
+                    if (ret==1)
+                    {
+                        break;
+                    }
+                }
             }
         }
         //当棋盘下满后平局
-        else{
+        else
+        {
             printf("平局\n");
         }
     }
 }
-void menu(){
+void menu()
+{
     int choose,x;
-    do
-    {
+    do{
         printf("###########################\n");
         printf("######1.Play 0.Exit #######\n");
         printf("###########################\n");
@@ -192,11 +232,12 @@ void menu(){
         switch (choose)
         {
         case 1:
-            //进行游戏
-            game();
+            //加载游戏
+            Loading(2);
+            //game();
             break;
         case 0:
-            //退出游戏动画，待写
+            Loading(1);
             break;
         default:
             printf("请输入正确的数字:\n");
@@ -205,6 +246,7 @@ void menu(){
     } while (choose);
     
 }
-int main(){
+int main()
+{
     menu();
 }
