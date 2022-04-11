b28ced0c7f6dad68b491224180c03b61c336797d
Awww-nanguasu <2328194470@qq.com>
2022-04-11 21:22:05 +0800

    数据存储的小练习

---
+++ base-c/c_04_11.c
@@ -0,0 +1,18 @@
+#include<string.h>
+#include<stdio.h>
+/*int main(){
+    char a[1000];
+    int i;
+    for ( i = 0; i < 1000; i++)
+    {
+        a[i]=-1-i;
+    }
+    printf("%d\n",strlen(a));
+}*/
+unsigned char i =0;
+int main(){
+    for(i=0;i<=255;i++)
+    {
+        printf("Helloworld\n");
+    }
+}
