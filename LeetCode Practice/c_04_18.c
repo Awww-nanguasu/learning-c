979b4e86cf320c60e0867135ff7d469fa9d15234
Awww-nanguasu <2328194470@qq.com>
2022-04-18 20:18:52 +0800

    力扣第28题

---
+++ LeetCode Practice/c_04_18.c
@@ -0,0 +1,22 @@
+//Á¦¿ÛµÚ28Ìâ
+/*int strStr(char * haystack, char * needle){
+    int i,j,first;
+    if(needle[0]=="")
+        return 0;
+    for(j=0,i=0;haystack[i]!=0;i++)
+    {
+        if(haystack[i]==needle[0])
+        {
+            for(j=1;needle[j]!=0 ;j++)
+            {
+                if(needle[j]!=haystack[i+j])
+                    break;
+            }
+            if(needle[j]==0)
+            {
+                return i;
+            }
+        }
+    }
+    return -1;
+}*/
