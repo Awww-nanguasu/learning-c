a0c531dfa6dc9d0131853bd1b65215d8ed5b6ead
Awww-nanguasu <2328194470@qq.com>
2022-04-24 12:02:13 +0800

    力扣第69题

--- LeetCode Practice/c_04_23.c
+++ LeetCode Practice/c_04_23.c
@@ -1,25 +1,17 @@
 #include<stdio.h>
 #include<math.h>
-/*int Solution(float x1,float x){
-    return x1*x1-x;
-}
-float  integal(float x1,float x){
-    x1=(x1*x1+x)/(2*x1);
-    return x1;
-}
-int mySqrt(int x){
-    if(x==1){
-        return 1;
+/*int mySqrt(int x){
+    if(x==0){
+        return 0;
     }
-
-    float x1;
-    for (x1=(float)x/2;Solution(x1,x)<0;x1++){
-        ;
-    }
-    while (Solution(x1,x)>1.0E-12)
+    double x1,x0=x;
+    while (1)
     {
-        ;
-        x1=integal(x1,x);
+        x0=x1;
+        x1=0.5*x0+x/(2*x0);
+        if(fabs(x1-x0)<1E-12){
+            break;
+        }
     }
-    return (int)x1;
-}/*
+    return (int)x0;
+}*/
